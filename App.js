fb6652b39cf4ef439d69e5f922b8288821185317
josias <kologojosias@gmail.com>
2019-09-24 02:32:39 +0100

    Functionalities done

---
+++ App.js
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
+
+const App = () => {
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const configureNewGmeHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  };
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  const gameOverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds);
+  };
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+  if (userNumber && guessRounds <= 0) {
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
+  }
+  else if (guessRounds > 0) {
+    content = <GameOverScreen 
+                  roundsNumber={guessRounds} 
+                  userNumber={userNumber} 
+                  onRestart={configureNewGmeHandler}
+              />;
+  }
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
+      {content}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
